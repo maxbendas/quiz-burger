3b3ced09c9514750a6fca14498c0d16ff0c893af
Max <maxbendas86@gmail.com>
2020-05-16 22:13:52 +0300

    2 day

--- script/script.js
+++ script/script.js
@@ -3,7 +3,83 @@ const modalBlock = document.querySelector('#modalBlock');
 const closeModal = document.querySelector('#closeModal');
 const questionTitle = document.querySelector('#question');
 const formAnswers = document.querySelector('#formAnswers');
+const nextButton = document.querySelector('#next');
+const prevButton = document.querySelector('#prev');
 
+const questions = [
+  {
+    question: "Какого цвета бургер?",
+    answers: [
+      {
+        title: 'Стандарт',
+        url: './image/burger.png'
+      },
+      {
+        title: 'Черный',
+        url: './image/burgerBlack.png'
+      }
+    ],
+    type: 'radio'
+  },
+  {
+    question: "Из какого мяса котлета?",
+    answers: [
+      {
+        title: 'Курица',
+        url: './image/chickenMeat.png'
+      },
+      {
+        title: 'Говядина',
+        url: './image/beefMeat.png'
+      },
+      {
+        title: 'Свинина',
+        url: './image/porkMeat.png'
+      }
+    ],
+    type: 'radio'
+  },
+  {
+    question: "Дополнительные ингредиенты?",
+    answers: [
+      {
+        title: 'Помидор',
+        url: './image/tomato.png'
+      },
+      {
+        title: 'Огурец',
+        url: './image/cucumber.png'
+      },
+      {
+        title: 'Салат',
+        url: './image/salad.png'
+      },
+      {
+        title: 'Лук',
+        url: './image/onion.png'
+      }
+    ],
+    type: 'checkbox'
+  },
+  {
+    question: "Добавить соус?",
+    answers: [
+      {
+        title: 'Чесночный',
+        url: './image/sauce1.png'
+      },
+      {
+        title: 'Томатный',
+        url: './image/sauce2.png'
+      },
+      {
+        title: 'Горчичный',
+        url: './image/sauce3.png'
+      }
+    ],
+    type: 'radio'
+  }
+];
 
 btnOpenModal.addEventListener('click', () => {
   modalBlock.classList.add('d-block');
@@ -15,7 +91,8 @@ closeModal.addEventListener('click', () => {
 })
 
 document.addEventListener('click', (event) => {
-  if (!event.target.closest('.modal-dialog') &&
+  if (!event.target.closest('.modal-dialog')
+    &&
     !event.target.closest('.openModalButton')
   ) {
     modalBlock.classList.remove('d-block')
@@ -23,20 +100,48 @@ document.addEventListener('click', (event) => {
 })
 
 const playTest = () => {
-  const renderQuestions = () => {
-    questionTitle.textContent = 'Какого цвета бургер вы хотите?';
-    const name = 'Стандарт'
-    const img = './image/burger.png'
+  let numberQuestion = 0;
+  const renderAnswers = (index) => {
+    questions[index].answers.forEach((answer) => {
 
-    formAnswers.innerHTML = `<div class="answers-item d-flex flex-column">
-    <input type="radio" id="answerItem1" name="answer" class="d-none">
-    <label for="answerItem1" class="d-flex flex-column justify-content-between">
-      <img class="answerImg" src="${img}" alt="burger">
-      <span>${name}</span>
-    </label>
-  </div>
-  `
+      const answerItem = document.createElement('div');
+      answerItem.classList.add('answers-item', 'd-flex', 'flex-column')
+
+      answerItem.innerHTML = `
+              <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+              <label for="${answer.title}" class="d-flex flex-column justify-content-between">
+                <img class="answerImg" src="${answer.url}" alt="burger">
+                <span>${answer.title}</span>
+              </label>
+            `;
+      formAnswers.append(answerItem);
+    })
   }
-  renderQuestions()
+
+  const renderQuestions = (indexQestions) => {
+    formAnswers.innerHTML = '';
+
+    questionTitle.textContent = questions[indexQestions].question;
+
+    renderAnswers(indexQestions)
+  }
+  renderQuestions(numberQuestion);
+
+  const nextQuestion = () => {
+    numberQuestion++;
+    renderQuestions(numberQuestion);
+    // nextButton.removeEventListener('click', nextQuestion)
+  }
+
+  nextButton.onclick = nextQuestion
+
+  const prevQuestion = () => {
+    numberQuestion--;
+    renderQuestions(numberQuestion);
+    // prevButton.removeEventListener('click', prevQuestion)
+  }
+
+  prevButton.onclick = prevQuestion
+
 }
 
