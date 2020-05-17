7e60b5c8a58318e3760cbe87428472bbd34236f0
Max <maxbendas86@gmail.com>
2020-05-17 19:56:26 +0300

    3 day

--- script/script.js
+++ script/script.js
@@ -5,6 +5,8 @@ const questionTitle = document.querySelector('#question');
 const formAnswers = document.querySelector('#formAnswers');
 const nextButton = document.querySelector('#next');
 const prevButton = document.querySelector('#prev');
+const sendButton = document.querySelector('#send');
+const modalTitle = document.querySelector('.modal-title');
 
 const questions = [
   {
@@ -100,16 +102,20 @@ document.addEventListener('click', (event) => {
 })
 
 const playTest = () => {
+
+  const finalAnswers = [];
   let numberQuestion = 0;
+  const obj = {};
+  modalTitle.textContent = 'Ответь на вопрос:';
 
   const renderAnswers = (index) => {
     questions[index].answers.forEach((answer) => {
 
       const answerItem = document.createElement('div');
-      answerItem.classList.add('answers-item', 'd-flex', 'flex-column')
+      answerItem.classList.add('answers-item', 'd-flex', 'justify-content-center')
 
       answerItem.innerHTML = `
-              <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+              <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none" value="${answer.title}">
               <label for="${answer.title}" class="d-flex flex-column justify-content-between">
                 <img class="answerImg" src="${answer.url}" alt="burger">
                 <span>${answer.title}</span>
@@ -122,36 +128,82 @@ const playTest = () => {
   const renderQuestions = (indexQestions) => {
     formAnswers.innerHTML = '';
 
-    // ------DZ day 2
+    if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+      questionTitle.textContent = questions[indexQestions].question;
+      renderAnswers(indexQestions)
+      sendButton.classList.add('d-none')
+      prevButton.classList.remove('d-none')
+      nextButton.classList.remove('d-none')
+    }
 
     if (numberQuestion === 0) {
-      prevButton.style.display = 'none'
-    } else if (numberQuestion > 0) {
-      prevButton.style.display = 'inline-block'
+      prevButton.classList.add('d-none')
     }
 
-    if (numberQuestion === questions.length - 1) {
-      nextButton.style.display = 'none'
-    } else {
-      nextButton.style.display = 'inline-block'
+    if (numberQuestion === questions.length) {
+      questionTitle.textContent = '';
+      modalTitle.textContent = '';
+      nextButton.classList.add('d-none');
+      prevButton.classList.add('d-none');
+      sendButton.classList.remove('d-none');
+
+      formAnswers.innerHTML = `
+      <div class="form-group">
+         <label for="numberPhone">Enter your number</label>
+         <input type="phone" class="form-control" id="numberPhone" placeholder="phone">
+      </div>`
+
+      const numberPhone = document.querySelector('#numberPhone');
+      console.log(numberPhone);
+      numberPhone.addEventListener('input', (event) => {
+        event.target.value = event.target.value.replace(/[^0-9+-]/, "");
+      })
     }
 
-    // -------
 
-    questionTitle.textContent = questions[indexQestions].question;
 
-    renderAnswers(indexQestions)
+    if (numberQuestion === questions.length + 1) {
+      formAnswers.innerHTML = 'Спасибо за пройденный тест!';
+      sendButton.classList.add('d-none')
+
+      for (let key in obj) {
+        let newObj = {};
+        newObj[key] = obj[key];
+        finalAnswers.push(newObj)
+      }
+
+      setTimeout(() => {
+        modalBlock.classList.remove('d-block')
+      }, 2000)
+    }
+
   }
   renderQuestions(numberQuestion);
 
+  const checkAnswer = () => {
 
+    const inputs = [...formAnswers].filter((input) => input.checked || input.id === 'numberPhone')
+
+    inputs.forEach((input, index) => {
+
+      if (numberQuestion >= 0 && numberQuestion <= questions.length - 1) {
+        obj[`${index}_${questions[numberQuestion].question}`] = input.id
+      }
+
+      if (numberQuestion === questions.length) {
+        obj[`Номер телефона`] = input.value
+      }
+    })
+    // finalAnswers.push(obj)
+
+  }
 
   const nextQuestion = () => {
+    checkAnswer();
     numberQuestion++;
     renderQuestions(numberQuestion);
     // nextButton.removeEventListener('click', nextQuestion)
   }
-
   nextButton.onclick = nextQuestion
 
   const prevQuestion = () => {
@@ -159,8 +211,13 @@ const playTest = () => {
     renderQuestions(numberQuestion);
     // prevButton.removeEventListener('click', prevQuestion)
   }
-
   prevButton.onclick = prevQuestion
 
+  sendButton.onclick = () => {
+    checkAnswer();
+    numberQuestion++;
+    renderQuestions(numberQuestion);
+    console.log(finalAnswers);
+  }
 }
 
